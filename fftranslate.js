7fa8ff485c30281198d11061da0b5f45194d8ec8
Michele Bonazza <michele.bonazza@gmail.com>
2018-11-10 23:04:30 +0100

    Get bounding box of selection
    So that we can show a popup later on

--- fftranslate.js
+++ fftranslate.js
@@ -1,11 +1,19 @@
 const getTranslateUrl = text =>
   `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURI(text)}`;
 
+const showTranslation = (selection, translated) => {
+  window.alert(translated);
+  // TODO show a popup instead, here are the selection bounding box coords
+  const bounds = selection.getRangeAt(0).getBoundingClientRect();
+  console.log(`${bounds.left},${bounds.top} ${bounds.right},${bounds.bottom}`);
+};
+
 const onTranslate = m => {
-  const selected = window.getSelection().toString().trim();
-  if (selected)
-    fetch(getTranslateUrl(selected))
-      .then(r => r.json().then(v => window.alert(v[0][0][0])))
+  const selection = window.getSelection();
+  const selectedText = selection.toString().trim();
+  if (selectedText)
+    fetch(getTranslateUrl(selectedText))
+      .then(r => r.json().then(v => showTranslation(selection, v[0][0][0])))
       .catch(e => console.err(`rejected, ${e}`));
 };
 
