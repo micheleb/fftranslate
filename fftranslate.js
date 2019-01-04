56d1244e1de361409cdda09e81c266591a972da1
Michele Bonazza <michele@sketchtogether.com>
2019-01-04 15:14:55 +0100

    Show button on mouse up
    Hide on mouse down

--- fftranslate.js
+++ fftranslate.js
@@ -9,47 +9,64 @@ const showTranslation = (selection, translated) => {
 };
 
 const onTranslate = m => {
-  const selection = window.getSelection();
-  const selectedText = selection.toString().trim();
+  // console.log(`Translating: "${selectedText}"`);
   if (selectedText)
     fetch(getTranslateUrl(selectedText))
       .then(r => r.json().then(v => showTranslation(selection, v[0][0][0])))
       .catch(e => console.err(`rejected, ${e}`));
 };
 
-const onButtonClicked = e => {
-  e.preventDefault();
-  hideFFTranslateButtons();
-  onTranslate();
-};
-
-const onSelect = s => {
-  setTimeout(() => {
-    const bounds = window.getSelection().getRangeAt(0).getBoundingClientRect();
-    const absoluteLeft = bounds.left + window.scrollX;
-    const absoluteTop = bounds.top + window.scrollY;
-
-    const div = document.createElement('div');
-    div.id = 'fftranslate-button';
-    div.style.cssText = `top: ${absoluteTop - 32}px; left: ${absoluteLeft}px;`;
-    div.onclick = onButtonClicked;
-
-    const img = document.createElement('img');
-    img.id = 'fftranslate-img';
-    img.src = browser.extension.getURL('img/button.png');
-    div.appendChild(img);
-    document.body.appendChild(div);
-  }, 300);
-};
-
 const hideFFTranslateButtons = () => {
   const b = document.getElementById('fftranslate-button');
-  if (b) b.remove();
+  if (b) {
+    b.remove();
+  }
 };
 
+const onMouseDown = e => {
+  if (e.target.id !== 'fftranslate-img')
+    hideFFTranslateButtons();
+}
+
+const onButtonClicked = e => {
+  e.preventDefault();
+  onTranslate();
+  hideFFTranslateButtons();
+};
+
+const showButton = () => {
+
+  selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+
+  if (range.startOffset - range.endOffset === 0) {
+    return;
+  }
+
+  selectedText = selection.toString().trim();
+
+  const bounds = range.getBoundingClientRect();
+  const absoluteLeft = bounds.left + window.scrollX;
+  const absoluteTop = bounds.top + window.scrollY;
+  const buttonTop = Math.max(window.scrollY, absoluteTop - 40);
+
+  const div = document.createElement('div');
+  div.id = 'fftranslate-button';
+  div.style.cssText = `top: ${buttonTop}px; left: ${absoluteLeft}px;`;
+  div.onclick = onButtonClicked;
+
+  const img = document.createElement('img');
+  img.id = 'fftranslate-img';
+  img.src = browser.extension.getURL('img/button.png');
+  div.appendChild(img);
+  document.body.appendChild(div);
+}
+
+var selection;
+var selectedText;
 browser.runtime.onMessage.addListener(onTranslate);
-document.onselectstart = onSelect;
-document.onclick = hideFFTranslateButtons;
+document.onmousedown = onMouseDown;
+document.onmouseup = showButton;
 
 const head = document.getElementsByTagName('head')[0];
 const style = document.createElement('style');
@@ -59,6 +76,7 @@ const css = `
     position: absolute;
     line-height: 0;
     box-shadow: 0 3px 6px #ccc;
+    border-radius: 16px;
     z-index: 9999;
   }
   #fftranslate-button:hover {
@@ -67,6 +85,9 @@ const css = `
   #fftranslate-img {
     width: 32px;
     height: 32px;
+    background-color: white;
+    padding: 2px;
+    border-radius: 16px;
   }
 `;
 style.appendChild(document.createTextNode(css));
