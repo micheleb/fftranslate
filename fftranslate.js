53574aff6ca4780d1a57d9e993eefac44036ea0f
Michele Bonazza <michele.bonazza@gmail.com>
2019-04-24 22:00:55 +0200

    Set popup minimum left position to 0

--- fftranslate.js
+++ fftranslate.js
@@ -7,21 +7,23 @@ let mouseDownEvent;
 let translating;
 
 const getTranslateUrl = text =>
-  `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURI(text)}`;
+  `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURI(
+    text
+  )}`;
 
 const showTranslation = (selection, translated) => {
   // window.alert(translated);
-  hide('button');
+  hide("button");
   const bounds = selection.getRangeAt(0).getBoundingClientRect();
   const top = bounds.top + window.scrollY;
   const bottom = bounds.bottom + window.scrollY;
   const left = bounds.left + window.scrollX;
 
-  const div = document.createElement('div');
-  div.id = 'fftranslate-popup';
+  const div = document.createElement("div");
+  div.id = "fftranslate-popup";
   div.innerHTML = translated;
   // add the (invisible) popup to the DOM to measure it...
-  div.style.cssText = 'opacity: 0;';
+  div.style.cssText = "opacity: 0;";
   document.body.appendChild(div);
 
   // so that we can center it with the selection
@@ -30,7 +32,7 @@ const showTranslation = (selection, translated) => {
   const over = top - h >= window.scrollY;
   // ...making it visible at the same time
   div.style.cssText = `opacity: 1; top: ${over ? top - h : bottom + 10}px;
-    left: ${left + (bounds.right - bounds.left) / 2 - w / 2}px;
+    left: ${Math.max(0, left + (bounds.right - bounds.left) / 2 - w / 2)}px;
   `;
 };
 
@@ -40,33 +42,34 @@ const onTranslate = m => {
   const url = getTranslateUrl(selectedText);
   if (selectedText)
     fetch(url)
-      .then(r => r.json().then(v => {
-        // google splits the translated text into sentences, for some reason...
-        const translated = v[0].map(t => t[0])
-          .reduce((acc, cur) => `${acc} ${cur}`, '');
-        showTranslation(selection, translated);
-      }))
+      .then(r =>
+        r.json().then(v => {
+          // google splits the translated text into sentences, for some reason...
+          const translated = v[0]
+            .map(t => t[0])
+            .reduce((acc, cur) => `${acc} ${cur}`, "");
+          showTranslation(selection, translated);
+        })
+      )
       .catch(e => console.err(`rejected, ${e}`));
 };
 
 const hide = partialId => {
   const b = document.getElementById(`fftranslate-${partialId}`);
   if (b) b.remove();
-  if (partialId === 'popup') translating = false;
+  if (partialId === "popup") translating = false;
 };
 
 const onMouseDown = e => {
   mouseDownEvent = e;
-  if (e.target.id !== 'fftranslate-img')
-    hide('button');
-  if (e.target.id !== 'fftranslate-popup')
-    hide('popup');
-}
+  if (e.target.id !== "fftranslate-img") hide("button");
+  if (e.target.id !== "fftranslate-popup") hide("popup");
+};
 
 const onButtonClicked = e => {
   e.preventDefault();
   onTranslate();
-  hide('button');
+  hide("button");
 };
 
 const showButton = e => {
@@ -93,34 +96,36 @@ const showButton = e => {
   // possible, unless the cursor gets away from the selection
   const buttonTop = Math.max(
     window.scrollY,
-    selectingUp ?
-      Math.max(absoluteTop, pageY) - BTN_SIZE
-      : Math.min(absoluteBottom, pageY));
+    selectingUp
+      ? Math.max(absoluteTop, pageY) - BTN_SIZE
+      : Math.min(absoluteBottom, pageY)
+  );
 
-  const buttonLeft = pageX < absoluteLeft ?
-    Math.max(window.scrollX, Math.max(pageX, absoluteLeft) - BTN_SIZE)
-    : Math.min(absoluteRight, pageX);
+  const buttonLeft =
+    pageX < absoluteLeft
+      ? Math.max(window.scrollX, Math.max(pageX, absoluteLeft) - BTN_SIZE)
+      : Math.min(absoluteRight, pageX);
 
-  const div = document.createElement('div');
-  div.id = 'fftranslate-button';
+  const div = document.createElement("div");
+  div.id = "fftranslate-button";
   div.style.cssText = `top: ${buttonTop}px; left: ${buttonLeft}px;`;
   div.onclick = onButtonClicked;
 
-  const img = document.createElement('img');
-  img.id = 'fftranslate-img';
-  img.src = browser.extension.getURL('img/button.png');
+  const img = document.createElement("img");
+  img.id = "fftranslate-img";
+  img.src = browser.extension.getURL("img/button.png");
   div.appendChild(img);
   document.body.appendChild(div);
-}
+};
 
 browser.runtime.onMessage.addListener(onTranslate);
 document.onmousedown = onMouseDown;
 // delay showButton(), lest we get the stale selection on win.getSelection()
 document.onmouseup = e => setTimeout(() => showButton(e), 50);
 
-const head = document.getElementsByTagName('head')[0];
-const style = document.createElement('style');
-style.type = 'text/css';
+const head = document.getElementsByTagName("head")[0];
+const style = document.createElement("style");
+style.type = "text/css";
 const css = `
   #fftranslate-button {
     position: absolute;
